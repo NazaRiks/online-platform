440b2586e09fdacb7d1effdc08392295ebfc1c2b
micta <artemen23093@gmail.com>
2024-03-06 20:54:13 +0200

    final-changing

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ import CoursePage from "./Components/Contents/Courses/CoursePage.jsx";
 import RegisterPage from "./Components/Contents/register/register-page.jsx";
 import axios from "axios";
 import ProtectedRoute from "./Components/Defence/ProtectedRoute.jsx";
+import AdminMenu from "./Components/Contents/admin-menu/admin-menu.jsx";
+import CourseObject from "./Components/Contents/Courses/CourseObject.jsx";
+import ForumObject from "./Components/Contents/Forums/ForumObject.jsx";
 
 
 export default function App() {
@@ -45,16 +48,19 @@ export default function App() {
     return (
         <div className="app-container">
             <BrowserRouter>
-                {isTokenValid && <Header />}
+                {isTokenValid && <Header isAdmin={isAdmin} />}
                 <Routes>
                     <Route path="/auth" element={isTokenValid ? <Navigate to="/" /> : <AuthorizationPage />} />
                     <Route path="/" element={<ProtectedRoute isTokenValid={isTokenValid} component={WelcomePage} />} />
                     <Route path="/education" element={<ProtectedRoute isTokenValid={isTokenValid} component={MyEducationPage} />} />
                     <Route path="/courses" element={<ProtectedRoute isTokenValid={isTokenValid} component={CoursePage} />} />
+                    <Route path="/courses/course-object" element={<ProtectedRoute isTokenValid={isTokenValid} component={CourseObject} />} />
+                    <Route path="/courses" element={<ProtectedRoute isTokenValid={isTokenValid} component={CoursePage} />} />
                     <Route path="/statistic" element={<ProtectedRoute isTokenValid={isTokenValid} component={MyStatisticsPage} />} />
                     <Route path="/forums" element={<ProtectedRoute isTokenValid={isTokenValid} component={ForumPage} />} />
-                    <Route path="/admin" element={<ProtectedRoute isTokenValid={isTokenValid} component={ForumPage} />} />
-                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/forums/forum-object" element={<ProtectedRoute isTokenValid={isTokenValid} component={ForumObject} />} />
+                    <Route path="/admin" element={<ProtectedRoute isTokenValid={isAdmin && isTokenValid} component={AdminMenu} />} />
+                    <Route path="/register" element={<ProtectedRoute isTokenValid={isAdmin && isTokenValid} component={RegisterPage} />} />
                 </Routes>
             </BrowserRouter>
         </div>
